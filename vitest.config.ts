86e3d39eb87bad35ad513f01a81b740d83db22c2
nulltoken <emeric.fermas@gmail.com>
2026-05-05 13:54:44 +0200

    Easier identification of tests causing crashes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['test/lib/_vitest-setup.ts'],
     coverage: {
       exclude: ['test/**'],
     },
